c395097cfdeb70520545cbab3728f7930891bac8
jstn4012 <justin.belzile@polymtl.ca>
2024-02-10 16:15:53 -0500

    Utilisation de Pylint sur devoir1.py
    Utilisation de Pylint pour rendre le code conforme aux bonnes pratiques vues dans le cours

--- devoir1/devoir1.py
+++ devoir1/devoir1.py
@@ -1,130 +1,172 @@
+'''
 ##################################################
-## Run simulations (using solve_FICK.py)
+## Run simulations (using solve_fick.py)
 ##################################################
 ## Code to MEC8211
 ##################################################
-## Authors: 
+## Authors:
 ##     - Pablo ELICES PAZ
 ##     - Lucas BRAHIC
-##     - 
-## Date: 06/02/2024
+##     - Justin BELZILE
+## Date: 10/02/2024
 ##################################################
-## ToDo: 
+## ToDo:
 ##     - Check 2nd order solution
 ##     - Plot convergence
 ##################################################
+'''
 
-import numpy as np
-from numpy import linalg as LA 
-import solve_FICK
 import time
+import numpy as np
+from numpy import linalg as LA
 import matplotlib.pyplot as plt
+import solve_fick
 
+def plot_solutions(n_cases_ext, Order, file_name):
+    '''
+    Plots the solutions for different cases.
 
+    Args:
+        n_cases_ext (list): List of integers representing the number of cases to plot.
+        order (int): The order of the solutions (1 or 2).
+        file_name (str): The name of the file to save the plot.
 
-def plot_solutions(N_cases, order, file_name):
-
+    Returns:
+        tuple: A tuple containing the error values for each case.
+    '''
     # Numerical parameters
     dt = 1E5
     tol = 1E-15
     imax = 1E8
-    error_L1 = []
-    error_L2 = []
-    error_Linfini = []
+    error_l_1 = []
+    error_l_2 = []
+    error_l_infini = []
     print('runing solver...')
-    print(' order=', order)
+    print(' order=', Order)
     print('')
 
-    for N in N_cases:
-        print(N)
-        r, numSol, anSol = solve_FICK.solve(N+1, dt, order, imax, tol, debug = False)
-        errorL1 = LA.norm(numSol-anSol)*1/N
-        errorL2 = LA.norm(numSol-anSol)*np.sqrt(1/N)
-        errorLinfini = np.max(np.abs(numSol - anSol))
-        print('N, errorL1,',N,  errorL1)
-        print('N, errorL2,',N,  errorL2)
-        print('N, errorLinfini,',N,  errorLinfini)
-        error_L1.append(errorL1)
-        error_L2.append(errorL2)
-        error_Linfini.append(errorLinfini)
-        plt.plot(r,numSol, label='N='+str(N))
-    
+    for n in n_cases_ext:
+        print(n)
+        r, num_sol, an_sol = solve_fick.solve(n+1, dt, Order, imax, tol, debug = False)
+        error_l1 = LA.norm(num_sol-an_sol)*1/n
+        error_l2 = LA.norm(num_sol-an_sol)*np.sqrt(1/n)
+        error_linfini = np.max(np.abs(num_sol - an_sol))
+        print('N, errorL1,',n,  error_l1)
+        print('N, errorL2,',n,  error_l2)
+        print('N, errorLinfini,',n,  error_linfini)
+        error_l_1.append(error_l1)
+        error_l_2.append(error_l2)
+        error_l_infini.append(error_linfini)
+        plt.plot(r,num_sol, label='N='+str(n))
 
-    plt.title('Order: '+str(order))
+    plt.title('Order: '+str(Order))
     plt.legend()
     plt.xlabel("r [m]")
     plt.ylabel("C [mol/m³]")
     plt.savefig(file_name)
     plt.show()
-    return error_L1,error_L2,error_Linfini
+    return error_l_1,error_l_2,error_l_infini
 
 time0 = time.time()
 
 # First order plot solution
-N_cases = [ 5, 10, 20, 40, 80]
-order = 1
-errors_values_1_L1,errors_values_1_L2,errorLinf_1 = plot_solutions(N_cases, order, file_name='solution_1stOrder.png')
+n_cases = [ 5, 10, 20, 40, 80]
+Order = 1
+errors_values_1_l1,errors_values_1_l2,error_linf_1 = plot_solutions(
+    n_cases, Order, file_name='solution_1stOrder.png')
 
-# Second order plot solution 
-N_cases = [ 5, 10, 20, 40, 80]
-order = 2
-errors_values_2_L1,errors_values_2_L2,errorLinf_2 = plot_solutions(N_cases, order, file_name='solution_2ndOrder.png')
+# Second order plot solution
+n_cases = [ 5, 10, 20, 40, 80]
+Order = 2
+errors_values_2_l1,errors_values_2_l2,error_linf_2 = plot_solutions(
+    n_cases, Order, file_name='solution_2ndOrder.png')
 
 print('time required: ', time.time() - time0)
 
-
-
 ## Convergence ordre 2 erreur L2 en fonction de delta_x
 
-h_values = [0.5/i for i in N_cases]   #résolution 
+h_values = [0.5/i for i in n_cases]   #résolution
 h_values.reverse()
-errorLinf_1.reverse()
-errorLinf_2.reverse()
-errors_values_1_L1.reverse()
-errors_values_2_L1.reverse()
-errors_values_1_L2.reverse()
-errors_values_2_L2.reverse()
+error_linf_1.reverse()
+error_linf_2.reverse()
+errors_values_1_l1.reverse()
+errors_values_2_l1.reverse()
+errors_values_1_l2.reverse()
+errors_values_2_l2.reverse()
 
-def plot_erreurs(errors_valuesL1,errors_valuesL2,errorLinf,order):
+def plot_erreurs(errors_values_l1,errors_values_l2,error_linf,Order):
+    '''
+    Plots the errors for different resolutions.
+
+    Args:
+        errors_values_l1 (list): List of L1 errors for each resolution.
+        errors_values_l2 (list): List of L2 errors for each resolution.
+        errorLinf (list): List of Linf errors for each resolution.
+        order (str): The order of the solutions ('1' or '2').
+
+    Returns:
+        None
+        '''
     plt.figure(figsize=(10, 6))
-    plt.plot(h_values, errorLinf, label='Erreur Linfini', color='blue', linewidth=2, marker='o')
-    plt.plot(h_values, errors_valuesL1, label='Erreur L1', color='red', linestyle='--', marker='x')
-    plt.plot(h_values, errors_valuesL2, label='Erreur L2', color='green', linestyle='-.', marker='s')
+    plt.plot(h_values, error_linf, label='Erreur l_infini',
+             color='blue', linewidth=2, marker='o')
+    plt.plot(h_values, errors_values_l1, label='Erreur l_1',
+             color='red', linestyle='--', marker='x')
+    plt.plot(h_values, errors_values_l2, label='Erreur l_2',
+             color='green', linestyle='-.', marker='s')
     plt.xlabel('Maillage en m^-1')
     plt.ylabel('Erreurs en mol/m^3')
-    plt.title('Comparaison des erreurs pour différentes résoltuions, pour ordre' + order)
+    plt.title('Comparaison des erreurs pour différentes résoltuions, pour ordre' + Order)
     plt.legend()
     plt.grid(True)
     plt.show()
 
-plot_erreurs(errors_values_1_L1,errors_values_1_L2,errorLinf_1, "1")
-#plot_erreurs(errors_values_2_L1,errors_values_2_L2,errorLinf_2, "1")
+plot_erreurs(errors_values_1_l1,errors_values_1_l2,error_linf_1, "1")
+#plot_erreurs(errors_values_2_l1,errors_values_2_l2,error_linf_2, "1")
 
+def plot_convergence(error_values, h_values_ext, Order) :
+    """
+    Plots the convergence of the error with respect to the grid size.
 
+    Args:
+        error_values (list): List of error values.
+        h_values_ext (list): List of grid sizes.
+        order (int): The order of the convergence (1 or 2).
 
+    Returns:
+        None
+    """
 
-def plot_convergence(error_values, h_values, order) :
-    
-
-    coefficients = np.polyfit(np.log(h_values[:3]), np.log(error_values[:3]), 1)
+    coefficients = np.polyfit(np.log(h_values_ext[:3]), np.log(error_values[:3]), 1)
     exponent = coefficients[0]
 
-    fit_function_log = lambda x: exponent * x + coefficients[1]
+    #fit_function_log = lambda x: exponent * x + coefficients[1]
 
-    fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
+    #fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
 
-    extrapolated_value = fit_function(h_values[-1])
+    def fit_function_log(x):
+        return exponent * x + coefficients[1]
+
+    def fit_function(x):
+        return np.exp(fit_function_log(np.log(x)))
+
+    extrapolated_value = fit_function(h_values_ext[-1])
 
     plt.figure(figsize=(8, 6))
-    plt.scatter(h_values, error_values, marker='o', color='b', label='Données numériques obtenues')
-    plt.plot(h_values, fit_function(h_values), linestyle='--', color='r', label='Régression en loi de puissance')
+    plt.scatter(h_values_ext, error_values, marker='o',
+                color='b', label='Données numériques obtenues')
+    plt.plot(h_values_ext, fit_function(h_values_ext), linestyle='--',
+             color='r', label='Régression en loi de puissance')
 
-    plt.scatter(h_values[-1], extrapolated_value, marker='x', color='g', label='Extrapolation')
+    plt.scatter(h_values_ext[-1], extrapolated_value, marker='x', color='g', label='Extrapolation')
 
-    plt.title('Convergence d\'ordre' + order + '\n de l\'erreur $L_2$ en fonction de $Δx$',
-          fontsize=14, fontweight='bold', y=1.02)  # Le paramètre y règle la position verticale du titre
+    plt.title('Convergence d\'ordre' + Order +
+              '\n de l\'erreur $L_2$ en fonction de $Δx$',
+          fontsize=14, fontweight='bold', y=1.02)
+    # Le paramètre y règle la position verticale du titre
 
-    plt.xlabel('Taille de maille $h_{max}$ ou $Δx$ (cm)', fontsize=12, fontweight='bold')  # Remplacer "h" par "Δx"
+    plt.xlabel('Taille de maille $h_{max}$ ou $Δx$ (cm)',
+               fontsize=12, fontweight='bold')  # Remplacer "h" par "Δx"
     plt.ylabel('Erreur $L_2$ (m/s)', fontsize=12, fontweight='bold')
 
     plt.gca().spines['bottom'].set_linewidth(2)
@@ -134,20 +176,16 @@ def plot_convergence(error_values, h_values, order) :
     plt.tick_params(width=2, which='both', direction='in', top=True, right=True, length=6)
 
     equation_text = f'$L_2 = {np.exp(coefficients[1]):.4f} \\times Δx^{{{exponent:.4f}}}$'
-    equation_text_obj = plt.text(0.05, 0.05, equation_text, fontsize=12, transform=plt.gca().transAxes, color='k')
+    equation_text_obj = plt.text(0.05, 0.05, equation_text, fontsize=12,
+                                 transform=plt.gca().transAxes, color='k')
 
     equation_text_obj.set_position((0.5, 0.4))
-
 
     plt.xscale('log')
     plt.yscale('log')
     plt.grid(True)
     plt.legend()
     plt.show()
-    
-plot_convergence(errors_values_1_L2, h_values, "1")
-#plot_convergence(errors_values_2_L2, h_values, "2")
 
-
-
-
+plot_convergence(errors_values_1_l2, h_values, "1")
+#plot_convergence(errors_values_2_l2, h_values, "2")

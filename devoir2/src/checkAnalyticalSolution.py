8d14cff33fef48753434c81aafccbe70c6fabefc
pelicesPolymtl <157422704+pelicesPolymtl@users.noreply.github.com>
2024-03-04 21:12:13 -0500

    Create checkAnalyticalSolution.py

---
+++ devoir2/src/checkAnalyticalSolution.py
@@ -0,0 +1,15 @@
+import numpy as np
+import solve_FICK_sourceTerm as mycode
+import matplotlib.pyplot as plt
+
+n = 20
+tFinal = 20
+for t in np.linspace(0,tFinal,n+1):
+    # Geometry
+    r0 = 0
+    rf = 0.5
+    r = np.linspace(r0, rf, n)
+    c_an = mycode.manufactured_solution(r, t)
+    plt.plot(r, c_an, label='time t='+str(t))
+plt.legend()
+plt.show()

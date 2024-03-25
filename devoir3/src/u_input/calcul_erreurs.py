405e4c6b4f3447ef0ea6aa61fafb845606a532b8
jstn4012 <158000347+jstn4012@users.noreply.github.com>
2024-03-24 21:42:54 -0400

    Update calcul_erreurs.py

--- devoir3/src/u_input/calcul_erreurs.py
+++ devoir3/src/u_input/calcul_erreurs.py
@@ -8,15 +8,20 @@
 u_num = 0.1177
 
 # u_input
-u_input = 10.31 ** 0.5
+u_input_gauche = 7.76 ** 0.5
+u_input_droite = 11.21 ** 0.5
 
 # u_D
 sr = 14.7
 br = 10.0
 u_d = (sr ** 2 + br ** 2) ** 0.5
 
-u_val = (u_num ** 2 + u_input ** 2 + u_d ** 2) ** 0.5
-print(f"L'erreur u_val est de {u_val:.2f} micrometres^2")
+u_val_gauche = (u_num ** 2 + u_input_gauche ** 2 + u_d ** 2) ** 0.5
+u_val_droite = (u_num ** 2 + u_input_droite ** 2 + u_d ** 2) ** 0.5
+
+print(f"L'erreur u_val a gauche est de {u_val_gauche:.2f} micrometres^2")
+print(f"L'erreur u_val a droite est de {u_val_droite:.2f} micrometres^2")
+
 
 # erreur de simulation E
 S = 25.1
@@ -29,8 +34,11 @@ k = 2
 
 # Inequation erreur modele
 # 𝐸 − ( 𝑘 𝑢_𝑣𝑎𝑙 ) ≤ 𝛿_𝑚𝑜𝑑𝑒𝑙 ≤ 𝐸 + ( 𝑘 𝑢_𝑣𝑎𝑙 )
-delta_mod_inf = E - k * u_val
-delta_mod_sup = E + k * u_val
+delta_mod_inf = E - k * u_val_gauche
+delta_mod_sup = E + k * u_val_droite
+
+delta_mod_milieu = (delta_mod_inf + delta_mod_sup) * 0.5
+delta_mod_erreur = (delta_mod_inf - delta_mod_sup) * 0.5
 
 print(f"L'erreur du modele est dans l'intervale : [{delta_mod_inf:.2f} ; {delta_mod_sup:.2f}] micrometres^2")
-print(f"Autrement dit, delta_model est ({E:.2f} ± {delta_mod_sup - E:.2f}) micrometres^2")
+print(f"Autrement dit, delta_model est ({delta_mod_milieu:.2f} ± {-delta_mod_erreur:.2f}) micrometres^2")
